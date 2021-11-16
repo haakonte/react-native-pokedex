a8c0d43285f0661d87e8487c978bbfd4a107ada6
Lavrans Kaul Sagen <lavrans.ks@gmail.com>
2021-11-16 15:08:46 +0100

    Infinite scroll

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { Button, Image, Text, View, ScrollView } from "react-native";
+import { Button, Image, Text, View, ScrollView, FlatList } from "react-native";
 import { fetchPokemon } from "../services/pokemon_api";
 import Pokeinfo from "./Pokeinfo";
 
 export default function Home({ navigation }) {
-  const [data, setData] = useState([]);
+  const [data, setData]: [any, React.Dispatch<React.SetStateAction<any>>] =
+    useState([]);
+  const [offset, setOffset] = useState(1);
+
+  const handleLoadMore = async () => {
+    await setOffset(offset + 1);
+    fetchPokemon(false, 20, 20 * offset).then((response) => {
+      setData([...data, ...response.data.allPokemon]);
+    });
+  };
+
   useEffect(() => {
     fetchPokemon(false, 20, 0).then((response) => {
       setData(response.data.allPokemon);
@@ -16,21 +26,29 @@ export default function Home({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home page boiler plate</Text>
-      <ScrollView>
-        {data.map((el: any) => (
-          <View key={el.id}>
+      <FlatList
+        data={data}
+        renderItem={({ item }: any) => (
+          <View key={item.id}>
             <Text>
-              {el.id}. {el.name}
+              {item.id}. {item.name}
             </Text>
             <Button
               title="Go to Details"
               onPress={() => {
-                navigation.navigate("Pokeinfo", { pokemon: el._id });
+                navigation.navigate("Pokeinfo", { pokemon: item._id });
               }}
             />
           </View>
-        ))}
-      </ScrollView>
+        )}
+        onEndReached={() => {
+          if (offset < 151 / 20) {
+            handleLoadMore();
+          }
+        }}
+        onEndReachedThreshold={0.5}
+        initialNumToRender={20}
+      />
     </View>
   );
 }
