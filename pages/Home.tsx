e91c2685163538c4c7173333f8abd9b9ab059858
Sven Herman Holmsen <svenhermanholmsen@gmail.com>
2021-11-17 15:26:05 +0100

    #3 styled colors to search bar

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -53,6 +53,8 @@ export default function Home({ navigation }) {
         style={styles.input}
         onChangeText={onChangeText}
         value={text}
+        placeholderTextColor="#ED6C02"
+        underlineColorAndroid="#ED6C02"
         placeholder="Search..."
         onSubmitEditing={() => {
           handleSearch();
@@ -103,10 +105,13 @@ const styles = StyleSheet.create({
   input: {
     height: 40,
     margin: 12,
-    borderWidth: 1,
+    borderWidth: 2,
     padding: 10,
     borderRadius: 5,
     width: "70%",
+    color: "#282c34",
+    borderColor: "#ED6C02",
+    backgroundColor: "white",
   },
 
   home: {
