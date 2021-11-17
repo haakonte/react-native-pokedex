74c8e30148228bc906c35099474a862833c3c967
Lavrans Kaul Sagen <lavrans.ks@gmail.com>
2021-11-17 13:38:48 +0100

    Søkefelt

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,18 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { Button, Image, Text, View, ScrollView, FlatList } from "react-native";
-import { fetchPokemon } from "../services/pokemon_api";
+import {
+  Button,
+  Image,
+  Text,
+  View,
+  ScrollView,
+  FlatList,
+  TextInput,
+  StyleSheet,
+} from "react-native";
+import { fetchPokemon, searchForPokemon } from "../services/pokemon_api";
 import Pokeinfo from "./Pokeinfo";
 
 export default function Home({ navigation }) {
   const [data, setData]: [any, React.Dispatch<React.SetStateAction<any>>] =
     useState([]);
   const [offset, setOffset] = useState(1);
+  const [text, onChangeText] = useState("");
+
+  const handleSearch = async () => {
+    setOffset(1);
+    searchForPokemon(text, 20, 0, false).then((response) => {
+      setData(response.data.searchForPokemon);
+    });
+  };
 
   const handleLoadMore = async () => {
     await setOffset(offset + 1);
-    fetchPokemon(false, 20, 20 * offset).then((response) => {
-      setData([...data, ...response.data.allPokemon]);
-    });
+    if (text != "") {
+      searchForPokemon(text, 20, 20 * offset, false).then((response) => {
+        setData([...data, ...response.data.searchForPokemon]);
+      });
+    } else {
+      fetchPokemon(false, 20, 20 * offset).then((response) => {
+        setData([...data, ...response.data.allPokemon]);
+      });
+    }
   };
 
   useEffect(() => {
@@ -26,6 +49,19 @@ export default function Home({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home page boiler plate</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="Search..."
+        onSubmitEditing={() => {
+          handleSearch();
+        }}
+        onFocus={() => {
+          onChangeText("");
+        }}
+        returnKeyType="search"
+      />
       <FlatList
         data={data}
         renderItem={({ item }: any) => (
@@ -52,3 +88,14 @@ export default function Home({ navigation }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+    width: "70%",
+  },
+});
